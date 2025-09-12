a29e1be4f6b7db7f9e97d73af03f178f7a244b34
ariebesehl <a.riebesehl@gmail.com>
2025-09-12 03:05:21 +0200

    build/refactor25254: create ZTM-SHARED-* components

--- ZTM/ZTM-BUILD.h
+++ ZTM/ZTM-BUILD.h
@@ -5,27 +5,6 @@
 #ifndef ZTM_BUILD_H_INCLUDED
 #define ZTM_BUILD_H_INCLUDED
 
-#if defined(__WIN64) || defined(_WIN64) || defined(WIN64)
-	#define ZTM_BUILD_WIDTH 32 // deliberate
-	#define ZTM_BUILD_WINDOWS
-#elif defined(__WIN32) || defined(_WIN32) || defined(WIN32)
-	#define ZTM_BUILD_WIDTH 32
-	#define ZTM_BUILD_WINDOWS
-#elif defined(__MACH__) || defined(__APPLE__)
-	#define ZTM_BUILD_WIDTH 32
-	#define ZTM_BUILD_APPLE
-#elif defined(__linux__)
-	#define ZTM_BUILD_WIDTH 32
-	#define ZTM_BUILD_LINUX
-#elif defined(__AVR)
-	#ifndef ZTM_BUILD_EMBED
-		#define ZTM_BUILD_EMBED 0x1
-	#endif // ZTM_BUILD_EMBED
-	#define ZTM_BUILD_WIDTH 8
-	#define ZTM_BUILD_AVR
-#else
-	#define ZTM_BUILD_WIDTH 32
-	#define ZTM_BUILD_UNKNOWN
-#endif // ZTM_BUILD_OS
+#include "ZTM-SHARED-BUILD.h"
 
 #endif // ZTM_BUILD_H_INCLUDED
---
+++ ZTM/ZTM-SHARED-BUILD.h
@@ -0,0 +1,31 @@
+/*** Copyright (C) 2019-2025 ZaidaTek, Andreas Riebesehl
+**** This work is licensed under: Creative Commons Attribution-NoDerivatives 4.0 International Public License
+**** For full license text, please visit: https://creativecommons.org/licenses/by-nd/4.0/legalcode
+***/
+#ifndef ZTM_SHARED_BUILD_H_INCLUDED
+#define ZTM_SHARED_BUILD_H_INCLUDED
+
+#if defined(__WIN64) || defined(_WIN64) || defined(WIN64)
+	#define ZTM_BUILD_WIDTH 32 // deliberate
+	#define ZTM_BUILD_WINDOWS
+#elif defined(__WIN32) || defined(_WIN32) || defined(WIN32)
+	#define ZTM_BUILD_WIDTH 32
+	#define ZTM_BUILD_WINDOWS
+#elif defined(__MACH__) || defined(__APPLE__)
+	#define ZTM_BUILD_WIDTH 32
+	#define ZTM_BUILD_APPLE
+#elif defined(__linux__)
+	#define ZTM_BUILD_WIDTH 32
+	#define ZTM_BUILD_LINUX
+#elif defined(__AVR)
+	#ifndef ZTM_BUILD_EMBED
+		#define ZTM_BUILD_EMBED 0x1
+	#endif // ZTM_BUILD_EMBED
+	#define ZTM_BUILD_WIDTH 8
+	#define ZTM_BUILD_AVR
+#else
+	#define ZTM_BUILD_WIDTH 32
+	#define ZTM_BUILD_UNKNOWN
+#endif // ZTM_BUILD_OS
+
+#endif // ZTM_SHARED_BUILD_H_INCLUDED
